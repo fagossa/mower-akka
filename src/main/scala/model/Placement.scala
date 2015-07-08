dad507473218c49e0ae8e3cd94351f66bfe91548
Fabian Gutierrez <fagossa@gmail.com>
2015-07-08 12:30:47 +0200

    Removes unapply

--- src/main/scala/model/Placement.scala
+++ src/main/scala/model/Placement.scala
@@ -2,7 +2,6 @@ package model
 
 sealed trait Instruction
 
-// Direction
 sealed trait Command extends Instruction
 
 case object Right extends Command
@@ -11,17 +10,7 @@ case object Left extends Command
 
 case object Forward extends Command
 
-object Command {
 
-  def unapply(dir: Char): Option[Command] = dir match {
-    case 'D' => Some(Right)
-    case 'G' => Some(Left)
-    case 'A' => Some(Forward)
-  }
-
-}
-
-// Orientation
 sealed trait Orientation extends Instruction
 
 case object North extends Orientation
@@ -31,14 +20,3 @@ case object East extends Orientation
 case object West extends Orientation
 
 case object South extends Orientation
-
-object Orientation {
-
-  def unapply(dir: Char): Option[Orientation] = dir match {
-    case 'N' => Some(North)
-    case 'E' => Some(East)
-    case 'W' => Some(West)
-    case 'S' => Some(South)
-  }
-
-}
